8f8f45fce5647f66064090df8fc7ba32e5be360b
Tommy MacWilliam <tommy@serenade.ai>
2019-08-28 15:49:47 -0700

    add app messages
    add messages that appear when the app isn't installed or running.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import * as vscode from "vscode";
 import CommandHandler from "./command-handler";
 import BaseApp from "./shared/app";
 import IPC from "./shared/ipc";
@@ -5,7 +6,24 @@ import Settings from "./shared/settings";
 
 export default class App extends BaseApp {
   private commandHandler?: CommandHandler;
-  private settings?: Settings;
+
+  showNotRunningMessage() {
+    vscode.window.showInformationMessage(
+      "Open the Serenade app to use Serenade with Visual Studio Code."
+    );
+  }
+
+  showInstallMessage() {
+    vscode.window
+      .showInformationMessage(
+        "Download the new Serenade app to use Serenade with Visual Studio Code.",
+        {},
+        "Download"
+      )
+      .then(() => {
+        vscode.env.openExternal(vscode.Uri.parse("https://serenade.ai/download"));
+      });
+  }
 
   start() {
     if (this.ipc) {
@@ -14,7 +32,7 @@ export default class App extends BaseApp {
 
     this.settings = new Settings();
     this.commandHandler = new CommandHandler(this.settings);
-    this.ipc = new IPC(this.commandHandler, 17376);
+    this.ipc = new IPC(this.commandHandler, 17376, () => {});
     this.commandHandler.pollActiveEditor();
 
     this.run();
